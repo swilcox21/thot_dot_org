71875c56a5baa7dc68d6e0cfd5ce057078e137ef
sam wilcox <swilcox21@gmail.com>
2023-08-20 13:35:56 -0400

    changes to Day to grab single day based on date passed in body

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 # from unicodedata import name
 from django.urls import include, path
 from rest_framework import routers
-from .views import UserViewSet, ReminderView, DayView, DailyView, MindsetView, ThotView
+from .views import UserViewSet, ReminderView, DayView, DaysView, DailyView, MindsetView, ThotView
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -15,6 +15,7 @@ urlpatterns = [
     path('reminder/<int:reminder_id>', ReminderView.as_view()),
     path('day/', DayView.as_view()),
     path('day/<int:day_id>', DayView.as_view()),
+    path('days/', DaysView.as_view()),
     path('daily/', DailyView.as_view()),
     path('daily/<int:daily_id>', DailyView.as_view()),
     path('mindset/', MindsetView.as_view()),
